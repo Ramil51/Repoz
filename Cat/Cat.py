757129107ecc6b632a41a34753d6748f39260ebe
Ramil51 <74852890+Ramil51@users.noreply.github.com>
2020-12-07 11:27:48 +0500

    Add files via upload

---
+++ Cat/Cat.py
@@ -0,0 +1,15 @@
+class Cat:
+    def __init__(self, name, age, sex):
+        self.name = name
+
+        self.age = age
+        self.sex = sex
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+    def getSex(self):
+        return self.sex
+
+

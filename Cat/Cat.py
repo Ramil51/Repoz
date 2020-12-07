f6492b486abef35911cc445d6f3a75b1fa07bf7b
Ramil51 <74852890+Ramil51@users.noreply.github.com>
2020-12-07 11:32:37 +0500

    Update Cat.py

--- Cat/Cat.py
+++ Cat/Cat.py
@@ -1,7 +1,6 @@
 class Cat:
     def __init__(self, name, age, sex):
         self.name = name
-
         self.age = age
         self.sex = sex
     def getName(self):
@@ -11,5 +10,6 @@ class Cat:
         return self.age
     def getSex(self):
         return self.sex
+        
 
 
